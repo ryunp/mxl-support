70df7935a4681b87928ee9161246f609eccdccb5
Ryan Paul <ryunpaul@gmail.com>
2018-07-09 02:29:21 -0700

    sysreport update
    added patch version check, better error handling

--- README.md
+++ README.md
@@ -1,28 +1,30 @@
 # MedianXL SupportTools
+## Helpful utilities during Median XL troubleshooting
 
-#### sys_report.bat output example:
-```
-D2 Installion Path: F:\Diablo II
+General feedback and discussion is facilitated at the [Median XL forum thread](http://forum.median-xl.com/viewtopic.php?p=188053).
 
-[DEP Settings]
-F:\Diablo II\D2VidTst.exe, ~ ~ RUNASADMIN WINXPSP3
-F:\Diablo II\Game.exe, ~ RUNASADMIN DWM8And16BitMitigation WINXPSP3
-F:\Diablo II\Diablo II.exe, ~ RUNASADMIN WINXPSP3
-F:\Diablo II\Median XL\Median XL.exe, ~ RUNASADMIN
+[d2mxl_sysreport.bat](#d2mxl_sysreport.bat)  
+[ps_ping.bat.bat](#ps_ping.bat)
 
-[File List]
-PlugY
-save
-Median XL
-support
-Crashdump
-bncache.dat
-SmackW32.dll
-Bnclient.dll
-D2Win.dll
-Storm.dll
-PlugY.dll
-binkw32.dll
-MXL.dll
-...
-```
+### d2mxl_sysreport.bat
+Reports various D2 and OS settings.
+
+##### Script Activities
+* Installation Path (Registry)
+* patch_d2.mpq Version (MD5 via CertUtil: win7+)
+* D2 DEP Settings (Registry)
+* D2 Root File List
+
+##### Example
+![GIF Example](media/d2mxl_sysreport.gif)
+
+### ps_ping.bat
+Offers a textual menu interface for pinging network connections associated with running programs.
+
+##### Script Activities
+* Select Process
+* Select Network Connection
+* Ping target IP
+
+##### Example
+![GIF Example](media/ps_ping.gif)
--- d2mxl_sysreport.bat
+++ d2mxl_sysreport.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 REM Author: Ryan Paul
-REM Date: 07/02/18
-REM Usage: mxl_report.bat [process name]
-REM Description: Scans various OS settings and file system paths related to D2
+REM Date: 07/09/18
+REM Version: 0.3
+REM Usage: d2mxl_sysreport.bat
+REM Description: Saves various D2 and OS settings report to a text file
 
 SETLOCAL EnableDelayedExpansion
 SETLOCAL EnableExtensions
@@ -10,18 +11,19 @@ SETLOCAL EnableExtensions
 
 
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-REM Temp file
+REM Report file
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 REM ~ Define output temp file ~
-SET "output_file=%tmp%\d2mxl_report.txt"
+SET "output_file=%~n0.txt"
 
-REM ~ Test for temp file permissions ~
+REM ~ Test for report text file permissions ~
 TYPE NUL > "%output_file%"
 IF EXIST "%output_file%" (
     SET "tmpfile_exists=TRUE"
 ) ELSE (
-    ECHO "Could not create temp file '%output_file%'^! Outputting to console..."
+    CALL :log "Could not create temp file '%output_file%'^! Outputting to console..."
+    CALL :log_nl
 )
 
 
@@ -32,12 +34,53 @@ REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 SET "reg_diabloII=HKEY_CURRENT_USER\Software\Blizzard Entertainment\Diablo II"
 
-FOR /F "tokens=2*" %%a in ('REG QUERY "%reg_diabloII%" /v "InstallPath"') DO SET "path_d2_install=%%b"
-IF "%path_d2_install: =%" == "" (
-    CALL :log "No Diablo II path found in registry^!"
+REM ~ Section header ~
+CALL :log "[Installation Path]"
+
+REM ~ Check for registry key ~
+REG QUERY "%reg_diabloII%" >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    FOR /F "tokens=2*" %%a IN ('REG QUERY "%reg_diabloII%" /v "InstallPath"') DO SET "path_d2_install=%%b"
+    IF "!path_d2_install: =!" == "" (
+        CALL :log "No Diablo II path found in registry^!"
+    ) ELSE (
+        CALL :log "'!path_d2_install!'"
+    )
 ) ELSE (
-    CALL :log "D2 Installion Path: %path_d2_install%"
+    CALL :log "No 'InstallPath' registry key detected!"
 )
+
+CALL :log_nl
+
+
+
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Patch Checksum
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SET "patchfile_MD5_113c=eb514785fc4666bd53d97ad69ff53d16"
+SET "patchfile_path=%path_d2_install%\patch_d2.mpq"
+
+REM ~ Section header ~
+CALL :log "[patch_d2.mpq Checksum]"
+
+REM ~ Check for MD5 hash util on system (Win7+) ~
+CertUtil >nul 2>&1 && (
+    REM ~ Exclude the verbose output text ~
+    FOR /F "skip=1 delims=" %%a IN ('CertUtil /hashfile "%patchfile_path%" MD5 ^| findstr /v "CertUtil"') DO SET "patch_checksum=%%a"
+    SET "patch_checksum=!patch_checksum: =!"
+    
+    REM ~ Check patch_d2.mpq version ~
+    IF "!patch_checksum!" == "!patchfile_MD5_113c!" (
+        SET "patch_result=v1.13c detected"
+    ) ELSE (
+        SET "patch_result=not v1.13c"
+    )
+    CALL :log "!patch_checksum! - !patch_result!"
+) || (
+    CALL :log "CertUtil.exe Unavailable"
+)
+
 CALL :log_nl
 
 
@@ -46,23 +89,23 @@ REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 REM DEP Settings
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+SET "reg_dep_path=HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
+
 REM ~ Section header ~
 CALL :log "[DEP Settings]"
 
-REM ~ Reg path ~
-SET "reg_dep_path=HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers"
-
-REM ~ Loop over each D2 reg entry ~
-FOR /F "delims=" %%a in ('REG QUERY "%reg_dep_path%" ^| FIND /I "%path_d2_install%"') DO (
+REM ~ Check if registry entries exists ~
+FOR /F "delims=" %%a IN ('REG QUERY "%reg_dep_path%" ^| FIND /I "%path_d2_install%"') DO (
     SET "match=%%a"
-    
+
     REM Strip extra spaces, split on reg entry type, assign to aray
     SET "match=!match:    =!"
     SET "match='!match:REG_SZ=', '!'"
 
-    REM Output info
     CALL :log "!match!"
 )
+IF "%match%"=="" CALL :log "No D2 registry key detected!"
+
 CALL :log_nl
 
 
@@ -71,14 +114,15 @@ REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 REM Diablo II File Listing
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-REM ~ Seciton header ~
-CALL :log "[File List]"
+REM ~ Section header ~
+CALL :log "[D2 Root File List]"
 
 REM ~ /B reduces output to only names, /O:E sorts by extension ~
-FOR /F "delims=" %%a in ('DIR /B /O:E "%path_d2_install%" ^| findstr /v /i "screenshot"') DO (
+FOR /F "delims=" %%a IN ('DIR /B /O:E "%path_d2_install%" ^| findstr /v /i "screenshot"') DO (
     REM Output info
     CALL :log "%%~a"
 )
+
 CALL :log_nl
 
 
@@ -89,7 +133,6 @@ REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 IF DEFINED tmpfile_exists (
     NOTEPAD %output_file%
-    DEL %output_file%
 ) ELSE (
     PAUSE
 )
@@ -111,16 +154,17 @@ REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 REM ~ Function signature: log(str) ~
 :log
     IF DEFINED tmpfile_exists (
-        ECHO %~1>> %output_file%
+        ECHO %~1>>"%output_file%"
     ) ELSE (
         ECHO %~1
     )
 EXIT /B 0
 
+
 REM ~ Outputs a newline ~
 :log_nl
     IF DEFINED tmpfile_exists (
-        ECHO\>> %output_file%
+        ECHO\>>"%output_file%"
     ) ELSE (
         ECHO\
     )
